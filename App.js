a71148b146af4330a02b80419c097efc14f39431
yogiditya23 <36948925+yogiditya23@users.noreply.github.com>
2018-03-25 21:09:56 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+//import Header from './src/Cuaca/Header';
+//import PrakiraanCuaca from './src/Cuaca/PrakiraanCuaca';
+//import VolumeBalok from './src/serahcai/VolumeBalok';
+import BelajarLayoutFix from './src/Layout/BelajarLayoutFix';
+
+
+export default class App extends Component {
+  render() {
+    return (
+    //  <View style={styles.containerMain}>
+      //  <Header headerText= {'Pendidikan Teknik Informatika'}/>
+        <BelajarLayoutFix />
+
+    //  </View>
+    );
+  }
+}
